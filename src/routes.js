0db9b670cf7ecdc4f0d4c8e9965119f7701b160d
Thiago Pereira <thiagorpereira7@gmail.com>
2019-11-02 19:28:06 -0300

    Insert configs and some adjusts

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 import { Router } from 'express'; // Importar apenas o Router e n o Express inteiro
+import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    message: 'Hello World',
-  })
-);
+routes.post('/users', UserController.store);
+
+// routes.get('/', async (req, res) => {
+//   const user = await User.create({
+//     name: 'Thiago Ribeiro',
+//     email: 'thiagorpereira7@gmail.com',
+//     password_hash: '12365465465',
+//   });
+
+//   return res.json(user);
+// });
 
 export default routes;
