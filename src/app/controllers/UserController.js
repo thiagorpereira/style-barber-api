e086d9891d485b634ac37dc677bad87e193ff92e
Thiago Pereira <thiagorpereira7@gmail.com>
2019-11-04 17:50:43 -0200

    Generate hash

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,8 +8,13 @@ class UserController {
       return res.status(400).json({ error: 'User already exists.' });
     }
 
-    const user = await User.create(req.body);
-    return res.json(user);
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    return res.json({ ok: true });
   }
 }
 
