00b9b2856e4fbdc47621eb3e7f18204f1b75a37d
Thiago Pereira <thiagorpereira7@gmail.com>
2019-11-22 11:26:16 -0200

    Insert initial  middlewares

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,15 @@ import { Router } from 'express'; // Importar apenas o Router e n o Express inte
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// All router after will pass to that middleware
+routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
