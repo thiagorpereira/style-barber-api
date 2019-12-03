40fbc25fb986e8e34cad7658a5d797be031671b5
Thiago Pereira <thiagorpereira7@gmail.com>
2019-12-03 12:09:41 -0200

    Create File model and migration and set relationship

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -8,19 +8,16 @@ import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
-
     const schema = Yup.object().shape({
       email: Yup.string()
         .email()
         .required(),
-      password: Yup.string()
-        .required()
+      password: Yup.string().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
-
 
     const { email, password } = req.body;
 
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -69,6 +69,7 @@ class UserController {
     }
 
     const { id, name, provider } = await user.update(req.body);
+
     return res.json({ id, name, email, provider });
   }
 }
