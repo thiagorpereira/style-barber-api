e8372ee861ef0fac41ae0796b23f6c39b8403bae
Thiago Pereira <thiagorpereira7@gmail.com>
2019-12-04 23:45:35 -0200

    List all provider schedule

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,18 +1,35 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
 import User from '../models/User';
+import Appointment from '../models/Appointment';
 
 class ScheduleController {
   async index(req, res) {
-    const checkUserProider = await User.findOne({
+    const checkUserProvider = await User.findOne({
       where: {
         id: req.userId,
         provider: true,
       },
     });
-    if (!checkUserProider) {
+    if (!checkUserProvider) {
       return res.status(400).json({ error: 'User is not a provider' });
     }
-    console.log(req.userId);
-    return res.json('hehe');
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
   }
 }
 
