200c9a331ba9ec01c21e4f8b11310ed69a345df9
Thiago Pereira <thiagorpereira7@gmail.com>
2019-12-09 10:34:54 -0200

    Notify when cancel appointment with email

---
+++ src/config/mail.js
@@ -0,0 +1,16 @@
+export default {
+  /**
+   * Protocol SMTP
+   */
+
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false, // use SSL
+  auth: {
+    user: '15f1116aab0f43',
+    pass: 'ffff31830b796b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
