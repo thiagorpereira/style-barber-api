bed847cf962cd03e505bf86e90e31d0073ba52df
Thiago Pereira <thiagorpereira7@gmail.com>
2019-12-10 08:31:29 -0200

    Config queue and build send email job

---
+++ src/lib/Queue.js
@@ -0,0 +1,45 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        // instance that connect with Bee
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle, // process job
+      };
+    });
+  }
+
+  add(queue, job) {
+    // queue = set queue of the job: CancelationMail, etc..
+    // job = Infos : appointments, etc..
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.on('failed', this.handleFailure).process(handle);
+    });
+  }
+
+  handleFailure(job, err) {
+    console.log(`Queue ${job.queue.name}: FAILED`, err);
+  }
+}
+
+export default new Queue();
