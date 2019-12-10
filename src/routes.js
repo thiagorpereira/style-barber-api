80737c4aab236dfc17a058d5c816c36ac8b1d099
Thiago Pereira <thiagorpereira7@gmail.com>
2019-12-10 08:33:16 -0200

    List available time of a provider

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController.js';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -24,6 +25,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
